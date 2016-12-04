741d340fdbf3019cc9fdbe703c382e516da46767
Yibo Chen <garychen529@gmail.com>
2016-12-04 16:29:37 -0500

    fixed an issue on the home page

---
+++ browser/js/common/directives/navbar/navbar-controller.js
@@ -0,0 +1,39 @@
+app.controller('NavbarCtrl', function($rootScope, $state, $scope, AuthService, AUTH_EVENTS) {
+    $scope.items = [
+        { label: 'Home', state: 'home' },
+        { label: 'About', state: 'about' },
+        { label: 'Challenges', state: 'challenges' },
+        { label: 'Create a Challenge', state: 'createchallenge', auth: true },
+        { label: 'Play Around!', state: 'test' },
+        { label: 'Profile', state: 'membersOnly', auth: true }
+    ];
+
+    $scope.user = null;
+
+    $scope.isLoggedIn = function() {
+        return AuthService.isAuthenticated();
+    };
+
+    $scope.logout = function() {
+        AuthService.logout().then(function() {
+            $state.go('home');
+        });
+    };
+
+    var setUser = function() {
+        AuthService.getLoggedInUser().then(function(user) {
+            $scope.user = user;
+        });
+    };
+
+    var removeUser = function() {
+        $scope.user = null;
+    };
+
+    setUser();
+
+    $rootScope.$on(AUTH_EVENTS.loginSuccess, setUser);
+    $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
+    $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
+
+});
