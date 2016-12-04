523763b2a3bb029ae9a462c199c1121f3ed80ee6
Michael Crowley <miketcrowley@gmail.com>
2016-12-04 18:41:21 -0500

    herokupush

--- browser/js/created/created.js
+++ browser/js/created/created.js
@@ -2,7 +2,7 @@ app.config(function($stateProvider){
 	$stateProvider.state('created', {
 		url: '/created/:id',
 		templateUrl: '/js/created/created.html',
-		resolve: {
+		resolve: { /** @ngInject */
 			challenge: function($stateParams, Challenge){
 				return Challenge.findOne($stateParams.id);
 			}
