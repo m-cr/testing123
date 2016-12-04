d54fc6ab8dc915bf56bdc76ebf61bb0e82544362
Yibo Chen <garychen529@gmail.com>
2016-12-04 16:01:13 -0500

    changed the navbar to a vertical layout and added hiding/unhiding functions

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -48,3 +48,15 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
     };
 
 });
+
+$(document).ready(function() {
+    $(".fa-times").click(function() {
+        $(".sidebar_menu").addClass("hide_menu");
+        $(".toggle_menu").addClass("opacity_one");
+    });
+
+    $(".toggle_menu").click(function() {
+        $(".sidebar_menu").removeClass("hide_menu");
+        $(".toggle_menu").removeClass("opacity_one");
+    });
+});
