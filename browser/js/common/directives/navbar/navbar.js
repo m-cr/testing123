5d601fe20a6a4431c81ae5553fa6194b7218dbd2
Michael Crowley <miketcrowley@gmail.com>
2016-12-05 15:00:15 -0500

    click or hover

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -11,24 +11,23 @@ app.directive('navbar', function() {
 
 $(document).ready(function() {
 
-    $(".toggle_menu").mouseover(function() {
+    var appear = function(){
         $(".sidebar_menu").animate({ 'margin-left':'0px' }, 50, () => {} );
         $(".toggle_menu").removeClass("opacity_one");
         $("#main").animate({ 'margin-left':'230px' }, 50,() => {} );
         $("#footer").animate({ 'margin-left':'230px' }, 400, () => {} );
-	});
+    };
 
-    $(".fa-times").click(function() {
+    var disappear = function(){
         $(".sidebar_menu").animate({ 'margin-left':'-230px' }, 50, () => {} );
         $(".toggle_menu").addClass("opacity_one");
         $("#main").animate({ 'margin-left':'0px' }, 50, () => {} );
         $("#footer").animate({ 'margin-left':'0px' }, 400, () => {} );
-    });
+    };
 
-    $(".sidebar_menu").click(function(){
-    	$(".sidebar_menu").animate({ 'margin-left':'-230px' }, 50, () => {} );
-        $(".toggle_menu").addClass("opacity_one");
-        $("#main").animate({ 'margin-left':'0px' }, 50, () => {} );
-        $("#footer").animate({ 'margin-left':'0px' }, 400, () => {} );
-    });
+    $(".toggle_menu").mouseover(appear).click(appear);
+
+    $(".fa-times").click(disappear);
+
+    $(".sidebar_menu").click(disappear);
 });
