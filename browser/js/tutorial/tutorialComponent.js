f3e0e361b49be93431681f87fa9e558eacfce29f
Michael Crowley <miketcrowley@gmail.com>
2016-12-05 14:24:58 -0500

    heroku push attempt

--- browser/js/tutorial/tutorialComponent.js
+++ browser/js/tutorial/tutorialComponent.js
@@ -1,39 +1,40 @@
 app.component('tutorial', {
 	bindings: {},
 	templateUrl: './js/tutorial/tutorial.html',
-	controller: function(Challenge, TutorialService){
-		
-		var that = this;
-		var challengeIndex = 2;
-
-		this.index = 0;
-
-		this.codeEditorOptions = TutorialService.codeEditorOptions;
-
-		this.bodyText = TutorialService.bodyText;
-
-		this.advance = function(){
-			challengeIndex = this.bodyText[this.index + 1].challengeId;
-			if(challengeIndex){
-				getChallenge();
-			}
-			return that.index++;
-		};
-
-		this.rewind = function(){
-			return that.index--;
-		};
-
-		var getChallenge = function(){
-			Challenge.findOne(challengeIndex)
-			  .then(function(challenge){
-			  	that.challenge = challenge;
-			  })
-			  .catch(function(err){
-			  	console.log(err);
-			  });
-		};
-
-     }
-
+	controller: 'TutorialCtrl'
 });
+
+app.controller('TutorialCtrl', ['Challenge', 'TutorialService', function(Challenge, TutorialService){
+
+	var that = this;
+	var challengeIndex = 2;
+
+	this.index = 0;
+
+	this.codeEditorOptions = TutorialService.codeEditorOptions;
+
+	this.bodyText = TutorialService.bodyText;
+
+	this.advance = function(){
+		challengeIndex = this.bodyText[this.index + 1].challengeId;
+		if(challengeIndex){
+			getChallenge();
+		}
+		return that.index++;
+	};
+
+	this.rewind = function(){
+		return that.index--;
+	};
+
+	var getChallenge = function(){
+		Challenge.findOne(challengeIndex)
+		  .then(function(challenge){
+		  	that.challenge = challenge;
+		  })
+		  .catch(function(err){
+		  	console.log(err);
+		  });
+	};
+}]);
+
